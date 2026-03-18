93f0c3d34ac577136ee8736b5eeeb08d1f5d0d9d
neehass <neele.hass@student.kit.edu>
2026-03-18 12:06:22 +0100

    auswertung veränderung timeseries Klassifikation

--- final-scripts/04_timeseries_results_diff_LC08-09.R
+++ final-scripts/04_timeseries_results_diff_LC08-09.R
@@ -10,6 +10,7 @@ library(sf)
 library(mapview)
 library(ranger)
 library(tidyr)
+library(cowplot)
 
 getwd()
 setwd("./Phosphate")
@@ -31,6 +32,7 @@ colnames(ind_loc) <- c("lon", "lat")
 # ---------------------------------------------------------
 # load predictions with WPI model ------------------
 # ---------------------------------------------------------
+# 2015 02.08. exluded !
 # REGRESSION -------------------
 name_RF <- "WPI_NORM"
 folder_PRED_WPI <- file.path(folder_PRED, name_RF)
@@ -79,6 +81,19 @@ cv_pred_REG <- app(rast_all_pred_WPI, function(x){
 col_fun <- colorRampPalette(c("white", "red"))
 png(file.path(output, "CV_PredictionTimeseries2013-2025.png"), height = 350, width = 300)
 plot(cv_pred_REG, col = col_fun(10), plg = list(title = "CV Predictions\n 2013-2025"))
+dev.off()
+
+# -----------------------------------------------------------
+# CV Classification ------------------
+# ---------------------------------------------------------
+rast_all_pred_WPI_class <- rast(all_pred_WPI_Class)
+
+cv_pred_Class<- app(rast_all_pred_WPI_class, function(x){
+  sd(x, na.rm=TRUE) / mean(x, na.rm=TRUE)
+})
+col_fun <- colorRampPalette(c("white", "red"))
+png(file.path(output, "CV_Class_PredictionTimeseries2013-2025.png"), height = 350, width = 300)
+plot(cv_pred_Class, col = col_fun(10), plg = list(title = "CV Predictions\n 2013-2025"))
 dev.off()
 
 # -----------------------------------------------------------
@@ -157,7 +172,7 @@ for(i in 1:length(index)){
 }
 
 # -----------------------------------------------------------
-# baseline 2013 difference PREDICTIONS ------------------
+# baseline 2013 difference PREDICTIONS Regression ------------------
 # ---------------------------------------------------------
 baseline2013 <- all_pred_WPI[[1]]
 diff <- func_baseline_diff(all_pred_WPI, baseline2013)
@@ -193,3 +208,114 @@ p_diff_reg <- ggplot(plot_df, aes(x = x, y = y, fill = value)) +
 ggsave(file.path(output, "diffPredciton_baseline2013.png"), p_diff_reg, 
           width = 4, height = 6, scale = 1.2)
 
+# -----------------------------------------------------------
+# baseline 2013 difference PREDICTIONS Klassifikation  ------------------
+# ---------------------------------------------------------
+baseline2013_class <- all_pred_WPI_Class[[1]]
+
+ind_loc <- data.frame(
+   name = c("Gabes", "Skhira","Sfax"),
+  lon = c(10.09544, 10.14889, 10.72389),
+  lat = c(33.91644, 34.34750, 34.70278)
+)
+ind_sf <- st_as_sf(ind_loc, coords = c("lon","lat"), crs = 4326)  # WGS84
+ind_sf_proj <- st_transform(ind_sf, crs = st_crs(baseline2013_class)$wkt)
+df_indloc <- cbind(ind_sf_proj, st_coordinates(ind_sf_proj))
+
+# comaprison 
+p_comp_class <- list()
+fixed_colors <- c( # chat gpt
+  "1 → 2" = "#fee5d9",
+  "1 → 3" = "#fcae91",
+  "1 → 4" = "#fb6a4a",
+  "1 → 5" = "#de2d26",
+  "2 → 1" = "#edf8fb",
+  "2 → 3" = "#b2e2e2",
+  "2 → 4" = "#66c2a4",
+  "2 → 5" = "#238b45",
+  "3 → 1" = "#f2f0f7",
+  "3 → 2" = "#cbc9e2",
+  "3 → 4" = "#9e9ac8",
+  "3 → 5" = "#6a51a3",
+  "4 → 1" = "#ffffcc",
+  "4 → 2" = "#c2e699",
+  "4 → 3" = "#78c679",
+  "4 → 5" = "#238443",
+  "5 → 1" = "#fef0d9",
+  "5 → 2" = "#fdcc8a",
+  "5 → 3" = "#fc8d59",
+  "5 → 4" = "#d7301f"
+)
+dir.create(file.path(output, "diffplots_propTab"))
+for(i in 1:(length(all_pred_WPI_Class)-1)){
+  year1 <- all_pred_WPI_Class[[i+1]]
+  compareGeom(baseline2013_class, year1)
+
+  df <- as.data.frame(year1, xy = TRUE)  # year1 Raster
+  df <- df[!is.na(values(baseline2013_class)) & !is.na(values(year1)), ]
+  names(df)[3] <- "pred"
+  df$baseline <- values(baseline2013_class)[!is.na(values(baseline2013_class))]
+  df <- df[complete.cases(df), ]
+
+  df$change <- df$baseline != df$pred
+  df_change <- df[df$change, ]
+  df_change <- na.omit(df_change)
+  df_change$transition <- paste(df_change$baseline, "→", df_change$pred)
+  unique(df_change$transition)
+
+  # library(RColorBrewer)
+  # par(mar=c(3,4,2,2))
+  # display.brewer.all()
+
+  # pal1 <- RColorBrewer::brewer.pal(8, "Blues")
+  # pal2 <- RColorBrewer::brewer.pal(8, "YlGn")
+  # pal3 <- rev(RColorBrewer::brewer.pal(4, "PuRd"))
+
+  # combined <- colorRampPalette(c(pal1, pal2, pal3))(
+  #   length(unique(df_change$transition))
+  # )
+  p <- ggplot(df_change, aes(x = x, y = y)) +
+     geom_tile(aes(fill = transition, color = transition), linewidth = 1) +  # geom_raster() +
+     scale_color_manual(values = fixed_colors) +
+    scale_fill_manual(values = fixed_colors) +
+      geom_point(data = df_indloc[1,], aes(x = X, y = Y), color = "red", size = 2) +
+    coord_equal() + 
+    theme_minimal() + 
+    theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 1)) + # wie terra)
+    labs(title = unique_years[i+1], 
+        color = "Class\ntransition", x = "", y= "", fill = "Class\ntransition")
+#  p
+  ggsave(file.path(output, "diffplots", paste0(unique_years[i+1], "_diffPredciton_CLASS_baseline2013.png")), p, 
+          width = 5, height = 6, scale = 1.2) #3 , 4
+
+  p_comp_class[[i]] <- p
+
+  prop_tab <- prop.table(table(df$baseline, df$pred), 1) # Kontingenztabelle (Konfusionsmatrix)
+  write.csv(prop_tab, file.path(output, "diffplots_propTab", 
+      paste0(unique_years[i+1], "_diffPredciton_CLASS_baseline2013.csv")))
+
+  if(i != (length(unique_years) -1)){
+    rm(df, df_change) # behalte letzte
+  }
+  
+}
+
+
+prop.table(table(df$baseline, df$pred), 1) # Kontingenztabelle (Konfusionsmatrix)
+
+
+
+# zusammenführen -----
+xlim <- range(df_change$x)
+ylim <- range(df_change$y)
+legend <- get_legend(p_comp_class[[2]])
+legend
+plots_no_legend <- lapply(p_comp_class, function(p) 
+  p + theme(legend.position = "none") +
+      coord_cartesian(xlim = xlim, ylim = ylim)  # keine coord_equal()
+)
+combined <- plot_grid(plotlist = plots_no_legend, ncol = 4, align = "hv")
+final <- plot_grid(combined, legend, ncol = 1, rel_heights = c(1, 0.1))
+# final
+ggsave(file.path(output, "diffPredciton_CLASS_baseline2013.png"), final, 
+          width = 5, height = 6, scale = 1.2)
